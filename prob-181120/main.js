9712c8171ef4efcdfc7ba546f2a3c7dace4aef5a
David Thong Nguyen <thnguyen2808@gmail.com>
2018-11-23 16:20:04 -0500

    Solved prob 1

--- prob-181120/main.js
+++ prob-181120/main.js
@@ -10,11 +10,22 @@ return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass ?
 */
 
-let arr = [10, 15, 3, 7];
-let k = 17;
+var arr = [10, 15, 3, 7, 9, 2];
+var k = 100;
 
-console.log(addUpTo(k, arr));
+console.log(addUpTo(arr, k));
 
-function addUpTo(k, arr) {
+function addUpTo(arr, k) {
+  for (var i = 0; i < arr.length; i++) {
+    // j = i + 1
+    // skips comparing with indices less than i
+    for (var j = i + 1; j < arr.length; j++) {
+      console.log({ i: arr[i], j: arr[j] });
+      if (arr[i] + arr[j] == k) {
+        return true;
+      }
+    }
+  }
+  // TODO: Bonus (what's a pass? like passing in 1 argument or passing an arg once?)
   return false;
 }
