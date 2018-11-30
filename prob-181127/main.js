6e18908b3b430aea61e4b3431827017029451d9a
David Thong Nguyen <dev@davidtn.com>
2018-11-30 15:31:30 -0500

    Working on it

--- prob-181127/main.js
+++ prob-181127/main.js
@@ -16,3 +16,24 @@ For example, the following tree has 5 unival subtrees:
    1   1 
 
 */
+
+// NOTE: subtrees with 1 element count as unival, since a node has the same value as itself
+
+function Node(val, left, right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function isUnival(root) {
+    return univalHelper(root, root.val);
+}
+
+function univalHelper(root, val) {
+    if (!root) {
+        return true;
+    }
+    if (root.val == val) {
+        return univalHelper(root.left, val) && univalHelper(root.right, val);
+    }
+}
