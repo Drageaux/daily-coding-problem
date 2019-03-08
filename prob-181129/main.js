f74c41bd23b2b5cbd9d44d5ce05be3bb15433403
David Thong Nguyen <dev@davidtn.com>
2019-03-08 10:16:26 -0800

    Done 29th Nov last year

---
+++ prob-181129/main.js
@@ -0,0 +1,25 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+*/
+function f() {
+  console.log('bruh');
+}
+
+function scheduler(f, n) {
+  var i = 0;
+  var start = new Date().getTime();
+
+  while (new Date().getTime() < start + n) {}
+  return f();
+  // setTimeout(f, n);
+}
+// This code blocks for the specified duration. This is CPU hogging code.
+// This is different from a thread blocking itself and releasing CPU cycles
+// to be utilized by another thread.No such thing is going on here.
+// Do not use this code, it's a very bad idea.
+
+scheduler(f, 1000);
